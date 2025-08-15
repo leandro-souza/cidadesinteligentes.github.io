b5e8ea7a6210103580367423643d6ba92cde0da4
Leandro Souza <leouneb@gmail.com>
2025-08-14 21:33:47 -0300

    Deletado versão anterior

--- includes/resource/options/typography_setting.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-
-    'title'         => esc_html__( 'Typography Settings', 'governlia' ),
-    'id'            => 'typography_setting',
-    'desc'          => '',
-    'icon'          => 'el el-edit'
-);
